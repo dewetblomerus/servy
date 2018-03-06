fbce22763ea2c7a0078194946d44ede27cc4841e
De Wet Blomerus <dewet@blomerus.org>
2018-03-06 10:25:44 +0200

    initial commit

---
+++ lib/servy/handler.ex
@@ -0,0 +1,55 @@
+require IEx
+
+defmodule Servy.Handler do
+  def handle(request) do
+    parse(request)
+    |> route
+    |> format_response
+  end
+
+  def parse(request) do
+    [method, path, _] = request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+    %{ method: method, path: path, resp_body: "" }
+  end
+
+  def route(conv) do
+    conv = %{ conv | resp_body: "Bats, Elepants, Hippos"}
+    # conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    #{conv.resp_body}
+    """
+  end
+end
+
+
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+expected_response = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 20
+
+Bears, Lions, Tigers
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
